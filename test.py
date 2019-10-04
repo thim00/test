7f50b14e612de182348629b5823dfbe901298f16
thim <thim00@yahoo.com>
2019-10-03 20:35:38 -0400

    Initial test.

---
+++ test.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+test = "Hello world!!"
+test_caps = test.upper()
+print(test)
+print(test_caps)
+
+
+numbers = [2, 4, 8]
+
+for number in numbers:
+    print(number)
+
+
+sieve = [True] * 101
+for i in range(2,100):
+    if sieve[i]:
+        print(i)
+        for j in range(i*i, 100, i):
+            sieve[j] = False
